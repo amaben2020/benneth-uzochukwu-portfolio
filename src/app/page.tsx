7aec969cbc215533ef566f082135d9162dd3f6dd
algomachine007 <amaben202phantom@gmail.com>
2024-01-19 08:55:28 +0100

    adding font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,41 @@
 "use client";
+import Card from "@/components/elements/card";
 import { useEffect, useRef } from "react";
 import styles from "./styles.module.css";
 export default function Home() {
   const maskRef = useRef<HTMLDivElement | null>(null);
-  useEffect(() => {
-    // Get a reference to the .mask element.
-    if (maskRef.current) {
-      // Add an event to catch mouse movements.
-      document.addEventListener("pointermove", (event) => {
-        // Calculate mouse position in percentages.
-        let x = Number((event?.clientX / window.innerWidth) * 100);
-        let y = Number((event?.clientY / window.innerHeight) * 100);
 
-        // Update the custom property values on the body.
-        maskRef.current?.style.setProperty("--mouse-x", x + "%");
-        maskRef.current?.style.setProperty("--mouse-y", y + "%");
-      });
-    }
+  useEffect(() => {
+    let animationFrameId: number | null;
+
+    const handleMouseMove = (event: any) => {
+      if (!animationFrameId) {
+        animationFrameId = requestAnimationFrame(() => {
+          let x = Number((event?.clientX / window.innerWidth) * 100);
+          let y = Number((event?.clientY / window.innerHeight) * 100);
+
+          maskRef.current?.style.setProperty("--mouse-x", x + "%");
+          maskRef.current?.style.setProperty("--mouse-y", y + "%");
+
+          animationFrameId = null;
+        });
+      }
+    };
+
+    document.addEventListener("pointermove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("pointermove", handleMouseMove);
+      cancelAnimationFrame(Number(animationFrameId));
+    };
   }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       {/* <Spotlight /> */}
       <section className={styles.mask} ref={maskRef}>
-        <div className="max-w-5xl mx-auto">
+        <div className="max-w-7xl mx-auto">
+          <Card />
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui sint
           placeat, dolorem quaerat laudantium, nulla corrupti velit nam optio
           temporibus magnam ipsa facilis ipsum, natus voluptatibus dolorum id
