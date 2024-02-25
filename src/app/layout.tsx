c0e2f07d8318a8fd0788930983728d84d364e80a
algomachine007 <amaben202phantom@gmail.com>
2024-02-25 19:13:14 +0100

    build fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
-import Transition from "./transition";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
