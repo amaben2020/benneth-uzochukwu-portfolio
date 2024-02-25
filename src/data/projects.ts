161d6b86c12c318fdc115ef01fe281e8c8f553b8
algomachine007 <amaben202phantom@gmail.com>
2024-02-25 19:16:03 +0100

    build fix

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,11 +1,9 @@
-import { TCard } from "@/components/elements/cards/hoverable";
-
-const PROJECTS: TCard[] & { url: "" } = [
-  {
-    title: "Cartstand",
-    image: "",
-    description: "",
-    subtitle: "",
-    url: "",
-  },
-];
+// const PROJECTS: TCard[] & { url: "" } = [
+//   {
+//     title: "Cartstand",
+//     image: "",
+//     description: "",
+//     subtitle: "",
+//     url: "",
+//   },
+// ];
